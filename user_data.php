cdcd312e21c132f667edd444c7a164565d86b63d
VerityO2 <114458775+VerityO2@users.noreply.github.com>
2022-10-21 18:14:57 +0100

    final touch

--- user_data.php
+++ user_data.php
@@ -1,15 +1,14 @@
 <?php
 if (isset($_POST['submit'])) {
     $fileToWriteTo = fopen("userdata.csv", "w");
-    $textToAdd = $_POST['name'] .  "..." . "\r\n" . $_POST['email'] .  "..." . "\r\n" . $_POST['dob'] .  "..."  .  "\r\n" . $_POST['country'] .  "..."  . "\r\n" . "\r\n";
+    $textToAdd = $_POST['name'] . "\r\n" . $_POST['email'] . "\r\n" . $_POST['dob'] . "\r\n" . $_POST['country'] . "\r\n" . "\r\n";
     fwrite($fileToWriteTo, $textToAdd);
     fclose($fileToWriteTo);
     
     $fl = file_get_contents("userdata.csv");
-    $fl2 = explode('...', $fl);
+    $fl2 = explode("\r\n", $fl);
     foreach ($fl2 as $key) {
         $newv =  $key . "<br>";
         print_r($newv);
     }
 }
-?>
